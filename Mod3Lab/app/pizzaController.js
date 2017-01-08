9c787a0832863bc873de25a05fb2ac002a807e3a
Gyokay Ali <gyokay.metin.ali@gmail.com>
2017-01-08 22:10:48 +0200

    Module 3 Homework
    Module 3 Homework added

--- Mod3Lab/app/pizzaController.js
+++ Mod3Lab/app/pizzaController.js
@@ -1,16 +1,18 @@
 app.controller('pizzaController', [
     '$scope',
     function ($scope) {
-        $scope.model = { 
+        $scope.model = {
             title: 'Pizza Builder',
             availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage',
-            'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'],
+                'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'],
             toppings: []
-         };
+        };
 
-        $scope.addTopping = function(topping){
+        $scope.addTopping = function (topping) {
             $scope.model.toppings.push(topping);
+            $scope.model.showAdded = true;
             $scope.model.search = null;
         }
+
     }
 ]);
